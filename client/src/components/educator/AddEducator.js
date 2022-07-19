4f084e4594534799d404a7dce094681d08774cfb
Noor Khorasi <nooruddin.khorasi@gmail.com>
2022-07-19 13:52:16 +0100

    fix minor styling issues

--- client/src/components/educator/AddEducator.js
+++ client/src/components/educator/AddEducator.js
@@ -7,17 +7,14 @@ export default function AddEducator({ handleSubmit }) {
         Educator Name:
         <input type="text" name="educatorName" />
       </label>
-      <hr />
       <label htmlFor="educatorEmail">
         Educator Email:
         <input type="text" name="educatorEmail" />
       </label>
-      <hr />
       <label htmlFor="educatorPosition">
         Educator Position:
         <input type="text" name="educatorPosition" />
       </label>
-      <hr />
       <button type="submit">Add Educator</button>
     </form>
   );
